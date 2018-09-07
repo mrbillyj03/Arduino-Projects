554ba5d828a4e8440bef2fe0ca2ce5aa5fdd599e
Bill Jenkins <41349914+mrbillyj03@users.noreply.github.com>
2018-09-07 15:22:08 -0500

    Add files via upload

--- 7219_Clock.ino
+++ 7219_Clock.ino
@@ -5,8 +5,12 @@
  * This sketch turns the MAX7219 array into a clock in 12-hour 
  * format with lowercase AM/PM indicator. Colon flashes at 1 Hz. 
  * Rev 08/16/2018
- * Fixed AM/PM bug, changed uppercase AM/PM indicator with 
- * lowercase.
+ * Fixed AM/PM bug, changed uppercase AM/PM indicator to 
+ * lowercase. Improved accuracy by subtracting 5 msec 
+ * from loop time. Clock is now accurate to within 
+ * several seconds a day.
+ * Rev. 09/07/2018
+ * Eliminated leading '0' on hour display if hr<10
 */
   
 #include <SPI.h>
@@ -22,6 +26,7 @@
 //  CS          D10
 //  CLK         D13
 
+// Hardware specific parameters
 int pinCS = 10; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
 int numberOfHorizontalDisplays = 4;
 int numberOfVerticalDisplays = 1;
@@ -29,14 +34,9 @@ int numberOfVerticalDisplays = 1;
 // declare instance of screen
 Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
 
-int pm, hr, minut, sec, dur;           // for (col,row) addressing of pixels, frequency of screen updates (in milliseconds)
-
-
-
 void setup() 
 {
-  // put your setup code here, to run once:
-  matrix.setIntensity(7); // Use a value between 0 and 15 for brightness
+  matrix.setIntensity(2); // Use a value between 0 and 15 for brightness
 
   // Adjust to your own needs
   matrix.setPosition(0, 0, 0); // The first display is at <0, 0>
@@ -47,52 +47,54 @@ void setup()
   matrix.setRotation(0, 1);    // rotate 90 deg clockwise
   matrix.setRotation(1, 1);    // rotate 90 deg clockwise
   matrix.setRotation(2, 1);    // rotate 90 deg clockwise
-  matrix.setRotation(3, 1);    // rotate 90 deg clockwise
-
-  // set initial time here
-  
-  hr=11;
-  minut=12;
-  sec=0;
-  pm=0;       // 0 for am, 1 for pm
-  randomSeed(analogRead(0));
+  matrix.setRotation(3, 1);    // rotate 90 deg clockwise 
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-  //Blink colon once a second
-  
-  matrix.fillScreen(LOW);
+  int hr=3;       // initial time settings go here
+  int minut=8;   
+  int sec=0;
+  int pm=1;       // even for am, odd for pm
  
-  matrix.drawChar(0,0,char((hr/10%10)+'0'),HIGH,LOW,1);
-  matrix.drawChar(6,0,char((hr%10)+'0'),HIGH,LOW,1);
-  matrix.drawPixel(12,2,HIGH);    // turn colon on
-  matrix.drawPixel(12,4,HIGH);
-  matrix.drawChar(14,0,char((minut/10%10)+'0'),HIGH,LOW,1);
-  matrix.drawChar(20,0,char((minut%10)+'0'),HIGH,LOW,1);
-  if (pm%2) 
-    matrix.drawChar(27,0,'p',HIGH,LOW,1);
-  else
-    matrix.drawChar(27,0,'a',HIGH,LOW,1);  
-  matrix.write();
-  delay(950);
-  matrix.drawPixel(12,2,LOW);   // turn colon off 
-  matrix.drawPixel(12,4,LOW);
-  matrix.write();
-  delay(45);      // 995 ms "dur" to make the clock more accurate
-  sec++;
-  if (sec==60)
+  while (1)       // this part loops forever
   {
-    sec=0;
-    minut++;
+    // Blink colon once a second, update time every 60 seconds
+    // clear screen
+    matrix.fillScreen(LOW);     
+    // draw hours
+    if (hr>9) matrix.drawChar(0,0,char((hr/10%10)+'0'),HIGH,LOW,1); 
+    matrix.drawChar(6,0,char((hr%10)+'0'),HIGH,LOW,1);
+    // draw colon 
+    matrix.drawPixel(12,2,HIGH);    
+    matrix.drawPixel(12,4,HIGH);
+    // draw minutes
+    matrix.drawChar(14,0,char((minut/10%10)+'0'),HIGH,LOW,1); 
+    matrix.drawChar(20,0,char((minut%10)+'0'),HIGH,LOW,1);
+    // draw am/pm indicator
+    if (pm%2) matrix.drawChar(27,0,'p',HIGH,LOW,1);
+    else matrix.drawChar(27,0,'a',HIGH,LOW,1);  
+    // draw screen
+    matrix.write();
+    delay(900);
+    // turn colon off 
+    matrix.drawPixel(12,2,LOW);   
+    matrix.drawPixel(12,4,LOW);
+    // draw screen
+    matrix.write();
+    delay(95);      // 995 ms "dur" to make the clock more accurate
+    sec++;          // calls to write to the 7219 take about 5 msec.
+    if (sec==60)    // set seconds back to zero, increment minute
+    {
+      sec=0;
+      minut++;
+    }
+    if (minut==60)  // set minutes back to zero, increment hours
+    {
+      minut=0;
+      hr++;
+    }
+    if (hr==12 && minut==0 && sec==0)pm++;    // at 12:00:00, toggle am/pm indicator
+    if (hr==13)hr=1;                          // set hr back to 1
   }
-  if (minut==60)
-  {
-    minut=0;
-    hr++;
-  }
-  if (hr==12 && minut==0 && sec==0)pm++;
-  if (hr==13)hr=1;
 }
-
